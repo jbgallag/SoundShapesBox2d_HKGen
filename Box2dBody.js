54885e84c4a30ae40f406af867f9694b3014d8b7
Brad Gallagher <jbradgallagher@gmail.com>
2020-11-23 13:12:21 -0700

    open syllables, haiku not enforced, badTones when not haiku #10

--- Box2dBody.js
+++ Box2dBody.js
@@ -238,7 +238,7 @@ Body.prototype.PauseTone = function (caller) {
 }
 
 Body.prototype.GetFreq = function(badTone) {
-    var halfSteps = [0,5,7,9,12];
+    var halfSteps = [0,5,9,12];
     var halfStepsTwo = [0,2,3,6,8,11];
     var freq = 0.0;
     if(badTone) {
