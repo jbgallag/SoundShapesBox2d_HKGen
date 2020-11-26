0c8e8b6450b0202cad50212e24a7d6ef62b3d4e5
Brad Gallagher <jbradgallagher@gmail.com>
2020-11-25 17:26:05 -0700

    multiple gains

--- sinewave.js
+++ sinewave.js
@@ -15,10 +15,14 @@ SineWave = function(context) {
   this.osc2 = this.context.createOscillator();
   this.osc3 = this.context.createOscillator();
   this.gain = this.context.createGain();
+  this.gain2 = this.context.createGain();
+  this.gain3 = this.context.createGain();
   this.gain.gain.value = 0;
+  this.gain2.gain.value = 0;
+  this.gain3.gain.value = 0;
   this.osc.connect(this.gain);
-  this.osc2.connect(this.gain);
-  this.osc3.connect(this.gain);
+  this.osc2.connect(this.gain2);
+  this.osc3.connect(this.gain3);
   this.osc.start(0);
     this.osc2.start(0);
     this.osc3.start(0);
@@ -53,6 +57,12 @@ SineWave.prototype.play = function() {
     this.gain.gain.setValueAtTime(0, this.context.currentTime);
     this.gain.gain.linearRampToValueAtTime(this.amplitude*1.5, 0.001 + this.context.currentTime);
     this.gain.gain.linearRampToValueAtTime(this.amplitude,  0.1 + this.context.currentTime);
+     this.gain2.gain.setValueAtTime(0, this.context.currentTime);
+    this.gain2.gain.linearRampToValueAtTime(this.amplitude*1.5, 0.001 + this.context.currentTime);
+    this.gain2.gain.linearRampToValueAtTime(this.amplitude,  0.1 + this.context.currentTime);
+     this.gain3.gain.setValueAtTime(0, this.context.currentTime);
+    this.gain3.gain.linearRampToValueAtTime(this.amplitude*1.5, 0.001 + this.context.currentTime);
+    this.gain3.gain.linearRampToValueAtTime(this.amplitude,  0.1 + this.context.currentTime);
     this.playing = true;
   }
 }
@@ -65,6 +75,14 @@ SineWave.prototype.pause = function() {
     this.gain.gain.value = this.amplitude;
     this.gain.gain.setValueAtTime(this.amplitude,  0.01 + this.context.currentTime);
     this.gain.gain.setTargetAtTime(0, this.context.currentTime+0.1, release);
+    this.gain2.gain.cancelScheduledValues(this.context.currentTime);
+    this.gain2.gain.value = this.amplitude;
+    this.gain2.gain.setValueAtTime(this.amplitude,  0.01 + this.context.currentTime);
+    this.gain2.gain.setTargetAtTime(0, this.context.currentTime+0.1, release);
+    this.gain3.gain.cancelScheduledValues(this.context.currentTime);
+    this.gain3.gain.value = this.amplitude;
+    this.gain3.gain.setValueAtTime(this.amplitude,  0.01 + this.context.currentTime);
+    this.gain3.gain.setTargetAtTime(0, this.context.currentTime+0.1, release);
     var self = this;
    // setTimeout(function() { self.fmosc.stop(0); self.osc.stop(0); }, 10*release*1000);
     this.playing = false;
