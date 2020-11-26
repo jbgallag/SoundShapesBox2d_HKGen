41187d9e6f40316eff958bb685b467a8916ecf19
Brad Gallagher <jbradgallagher@gmail.com>
2020-11-25 17:31:44 -0700

    multiple gains #2 (NO)

--- sinewave.js
+++ sinewave.js
@@ -12,20 +12,12 @@ SineWave = function(context) {
   this.amplitude = 0.1;
   this.playing = false;
   this.osc = this.context.createOscillator();
-  this.osc2 = this.context.createOscillator();
-  this.osc3 = this.context.createOscillator();
   this.gain = this.context.createGain();
-  this.gain2 = this.context.createGain();
-  this.gain3 = this.context.createGain();
   this.gain.gain.value = 0;
-  this.gain2.gain.value = 0;
-  this.gain3.gain.value = 0;
   this.osc.connect(this.gain);
-  this.osc2.connect(this.gain2);
-  this.osc3.connect(this.gain3);
   this.osc.start(0);
-    this.osc2.start(0);
-    this.osc3.start(0);
+  
+
   this.fmosc = this.context.createOscillator();
   this.fmosc.frequency.value = 4;
   this.fmosc.start(0);
@@ -52,17 +44,10 @@ SineWave.prototype.play = function() {
   if (!this.playing) {
     this.getOutNode().connect(this.context.destination);
     this.osc.frequency.value = this.frequency;
-      this.osc2.frequency.value = this.frequency*0.5;
-      this.osc3.frequency.value = this.frequency*2.0;
     this.gain.gain.setValueAtTime(0, this.context.currentTime);
-    this.gain.gain.linearRampToValueAtTime(this.amplitude*1.5, 0.001 + this.context.currentTime);
+    this.gain.gain.linearRampToValueAtTime(this.amplitude*1.5, 0.1 + this.context.currentTime);
     this.gain.gain.linearRampToValueAtTime(this.amplitude,  0.1 + this.context.currentTime);
-     this.gain2.gain.setValueAtTime(0, this.context.currentTime);
-    this.gain2.gain.linearRampToValueAtTime(this.amplitude*1.5, 0.001 + this.context.currentTime);
-    this.gain2.gain.linearRampToValueAtTime(this.amplitude,  0.1 + this.context.currentTime);
-     this.gain3.gain.setValueAtTime(0, this.context.currentTime);
-    this.gain3.gain.linearRampToValueAtTime(this.amplitude*1.5, 0.001 + this.context.currentTime);
-    this.gain3.gain.linearRampToValueAtTime(this.amplitude,  0.1 + this.context.currentTime);
+  
     this.playing = true;
   }
 }
@@ -75,14 +60,6 @@ SineWave.prototype.pause = function() {
     this.gain.gain.value = this.amplitude;
     this.gain.gain.setValueAtTime(this.amplitude,  0.01 + this.context.currentTime);
     this.gain.gain.setTargetAtTime(0, this.context.currentTime+0.1, release);
-    this.gain2.gain.cancelScheduledValues(this.context.currentTime);
-    this.gain2.gain.value = this.amplitude;
-    this.gain2.gain.setValueAtTime(this.amplitude,  0.01 + this.context.currentTime);
-    this.gain2.gain.setTargetAtTime(0, this.context.currentTime+0.1, release);
-    this.gain3.gain.cancelScheduledValues(this.context.currentTime);
-    this.gain3.gain.value = this.amplitude;
-    this.gain3.gain.setValueAtTime(this.amplitude,  0.01 + this.context.currentTime);
-    this.gain3.gain.setTargetAtTime(0, this.context.currentTime+0.1, release);
     var self = this;
    // setTimeout(function() { self.fmosc.stop(0); self.osc.stop(0); }, 10*release*1000);
     this.playing = false;
