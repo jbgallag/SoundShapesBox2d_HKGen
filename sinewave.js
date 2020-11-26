71620d86172b291beccbe03bcd4b526887271ce3
Brad Gallagher <jbradgallagher@gmail.com>
2020-11-26 10:47:12 -0700

    multiple gains #7 (NO)

--- sinewave.js
+++ sinewave.js
@@ -59,7 +59,7 @@ SineWave.prototype.pause = function() {
     this.gain.gain.cancelScheduledValues(this.context.currentTime);
     this.gain.gain.value = this.amplitude;
     this.gain.gain.setValueAtTime(this.amplitude,  0.01 + this.context.currentTime);
-    this.gain.gain.setTargetAtTime(0, this.context.currentTime+0.1, release);
+    this.gain.gain.setTargetAtTime(0, this.context.currentTime+0.01, release);
     var self = this;
    // setTimeout(function() { self.fmosc.stop(0); self.osc.stop(0); }, 10*release*1000);
     this.playing = false;
